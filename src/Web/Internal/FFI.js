9c3898dd2f1709c2cb46533b06393fb34cb73844
Maxime Dénès <mail@maximedenes.fr>
2018-08-16 14:29:59 +0200

    Fix #4: IE11 compatibility

--- src/Web/Internal/FFI.js
+++ src/Web/Internal/FFI.js
@@ -2,12 +2,14 @@
 
 exports._unsafeReadProtoTagged = function (nothing, just, name, value) {
   var obj = value;
+  var longname = "[object " + name + "]";
   while (obj != null) {
     var proto = Object.getPrototypeOf(obj);
     var ctor = proto.constructor.name;
-    if (ctor === name) {
+    var longctor = proto.constructor.toString();
+    if (ctor === name || longctor === longname) {
       return just(value);
-    } else if (ctor === "Object") {
+    } else if (ctor === "Object" || longctor === "[object Object]") {
       return nothing;
     }
     obj = proto;
