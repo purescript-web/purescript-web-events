8d9315c52489218692c45155de2a369e7c9eb850
Ping Chen <remotenonsense@gmail.com>
2019-12-17 18:26:43 +0900

    Do not rely on window in _unsafeReadProtoTagged

--- src/Web/Internal/FFI.js
+++ src/Web/Internal/FFI.js
@@ -1,13 +1,6 @@
 "use strict";
 
 exports._unsafeReadProtoTagged = function (nothing, just, name, value) {
-  if (typeof window !== "undefined") {
-    var ty = window[name];
-    if (ty != null && value instanceof ty) {
-      return just(value);
-    }
-    return nothing;
-  } 
   var obj = value;
   while (obj != null) {
     var proto = Object.getPrototypeOf(obj);
